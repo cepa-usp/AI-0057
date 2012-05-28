0ca375205ca7680542cc82d3031d07a619a263da
Mary Vigna <mavigna@gmail.com>
2012-05-28 11:34:04 -0300

    Fix botão TERMINEI não ativo + indentação do código JS

--- src/js/scorm-config.js
+++ src/js/scorm-config.js
@@ -8,6 +8,9 @@ var scorm = pipwerks.SCORM; // Seção SCORM
 scorm.version = "2004"; // Versão da API SCORM
 var PING_INTERVAL = 5 * 60 * 1000; // milissegundos
 var pingCount = 0;  // Conta a quantidade de pings enviados para o LMS
+var MAX_INIT_TRIES = 60;
+var init_tries = 0;
+var debug = true;
 
 // Inicia a AI.
 $(document).ready(function(){
@@ -34,17 +37,39 @@ $(document).ready(function(){
       }
   });
   
-  var xcoord = -3 + Math.floor(9 * Math.random());
-  var ycoord = -1 + Math.floor(5 * Math.random());
-  if (xcoord == 0 && ycoord == 0) ycoord = 1; // Só para não dar um vetor (0,0)
-  document.ggbApplet.setCoords('R2', xcoord, ycoord);
-
   $('#button1').button().click(evaluateExercise);
   $('#button2').button().click(evaluateExercise);
   $('#button3').button().click(reloadPage);
   
-  initAI();
+  checkCallbacks();
 });
+
+function checkCallbacks () {
+	var t2 = new Date().getTime();
+	var ok = false;
+	try {
+			// Sorteia as coordenadas do ponto C
+		var xcoord = -3 + Math.floor(9 * Math.random());
+		var ycoord = -1 + Math.floor(5 * Math.random());
+		if (xcoord == 0 && ycoord == 0) ycoord = 1; // Só para não dar um vetor (0,0)
+		document.ggbApplet.setCoords('R2', xcoord, ycoord);
+		ok = true;
+		message("ok");
+	}
+	catch(error) {
+		++init_tries;
+		
+		if (init_tries > MAX_INIT_TRIES) {
+			alert("Carregamento falhou.");
+		}
+		else {
+			message("falhou");
+			setTimeout("checkCallbacks()", 1000);
+		}
+	}
+	
+	if(ok) initAI();
+}
 
 //Refresh da Página.
 function reloadPage()
@@ -315,3 +340,12 @@ log.error = function (message) {
   }
 }
 
+// Mensagens de log
+function message (m) {
+	try {
+		if (debug) console.log(m);
+	}
+	catch (error) {
+		// Nada.
+	}
+}
