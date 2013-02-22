b3fbc0b4568c47a1a8a535658f2c70561faa94e0
Ivan Ramos Pagnossin <irpagnossin.edu@gmail.com>
2013-02-22 12:32:19 -0300

    Removido mensagens de debug.

--- deployment/js/scorm-config.js
+++ deployment/js/scorm-config.js
@@ -69,7 +69,6 @@ function checkCallbacks () {
 		if (xcoord == 0 && ycoord == 0) ycoord = 1; // Só para não dar um vetor (0,0)
 		document.ggbApplet.setCoords('R2', xcoord, ycoord);
 		ok = true;
-		message("ok");
 	}
 	catch(error) {
 		++init_tries;
@@ -78,7 +77,6 @@ function checkCallbacks () {
 			alert("Carregamento falhou.");
 		}
 		else {
-			message("falhou");
 			setTimeout("checkCallbacks()", 1000);
 		}
 	}
